3e8b6a9cb79295081e15ab0197a49490e6e14cc7
RuddyMo <ruddymorel10@gmail.com>
2025-11-06 21:40:11 +0100

    add todo

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col justify-center max-w-2xl mx-auto">
+    <div class='mb-2 text-right'>
+      <button class='cursor-pointer p-2'>
+        <Sun class="w-4 h-4" />
+      </button>
+    </div>
     <NuxtPage />
+    <p class='flex justify-center items-center gap-1 text-gray-500 p-2'>
+      source •<NuxtLink to="https://github.com/RuddyMo">@RuddyMo</NuxtLink>
+    </p>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { Sun } from "lucide-vue-next"
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/todo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class='w-full max-w-2xl mx-auto'>
+    <Card class='p-6'>
+        <CardHeader class='flex flex-row justify-between items-center p-0'>
+        <CardTitle>Todo List</CardTitle>
+        <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        </CardHeader>
+        <div class='flex items-center gap-2'>
+            <Input id="add" type="text" placeholder="Add a new task" />
+            <Button size="icon" class='cursor-pointer'>
+                <Plus class="w-4 h-4" />
+            </Button>
+        </div>
+        <ul class='mt-4 space-y-2'>
+            <li class='flex items-center justify-between p-2 border rounded'>
+            <span>Sample Task 1</span>
+            <Button size="icon" variant="ghost">X</Button>
+            </li>
+        </ul>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Plus } from 'lucide-vue-next';
+</script>
